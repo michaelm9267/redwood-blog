ad175cceaa6d267259561584bfbcaf922011c6d7
michaelm9267 <95662360+michaelm9267@users.noreply.github.com>
2023-12-12 19:38:39 -0600

    First Commit

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <header></header>
+      <main>This is the About Page</main>
+    </>
+  )
+}
+
+export default AboutPage
